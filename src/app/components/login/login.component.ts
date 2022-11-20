b8e6c52b996f50e92e36687d939a045e37b33de6
Carlos00-pixel <carlos.mayoral.00@gmail.com>
2022-11-20 20:46:15 +0100

    commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { Router } from '@angular/router';
+import { Login } from 'src/app/models/login';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,8 @@ export class LoginComponent implements OnInit {
   @ViewChild("username") userName !: ElementRef;
   @ViewChild("password") password !: ElementRef;
 
+  public user !: Login;
+
   constructor(
     private _service: LoginService,
     private _router: Router
@@ -22,13 +25,10 @@ export class LoginComponent implements OnInit {
 
   login(): void {
 
-    var user = {
-      userName: this.userName.nativeElement.value,
-      password: this.password.nativeElement.value
-    }
+    var user = new Login(this.userName.nativeElement.value, this.password.nativeElement.value);
 
-    this._service.login(user).subscribe(response => {
-      localStorage.setItem("token", response.response);
+    this._service.login(user).subscribe(data => {
+      localStorage.setItem("token", data.response);
       console.log(localStorage.getItem("token"));
       this._router.navigate(["/home"]);
     });
